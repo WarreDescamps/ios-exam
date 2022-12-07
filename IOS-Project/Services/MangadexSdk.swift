43938ae5b26de4d89fb0be1825fccda634676636
WarreDescamps <warre.descamps@student.vives.be>
2022-12-07 17:36:31 +0100

    Fix image loading in late or not loading in at all

--- IOS-Project/Services/MangadexSdk.swift
+++ IOS-Project/Services/MangadexSdk.swift
@@ -14,43 +14,40 @@ class MangadexSdk: ObservableObject {
         getManga(query: nil)
     }
     
-    private func convertToManga(_ results: Api.Types.Response.MangaSearch) {
-        var mangaArr = [Manga]()
-        
-        for result in results.data {
-            let manga = Manga(id: result.id,
-                              title: result.attributes.title.en,
-                              description: result.attributes.description.en,
-                              coverUrl: createCoverUrl(result.id))
-            mangaArr.append(manga)
-        }
-        
-        self.manga = mangaArr
-    }
-    
-    private func createCoverUrl(_ mangaId: String) -> String {
-        var url: String = ""
-        
+    private func appendWithCoverUrl(manga: Manga){
+        var manga = manga
         Api.Sdk.shared
-            .get(.cover(id: mangaId)) { (result: Result<Api.Types.Response.CoverLookup, Api.Types.Error>) in
-                switch result {
-                case .success(let success):
-                    url = "https://uploads.mangadex.org/covers/\(mangaId)/\(success.data.first(where: { cover in cover.attributes.locale == "en" })?.attributes.fileName ?? "")"
-                case .failure(let failure):
-                    print(failure.localizedDescription)
+            .get(.cover(id: manga.id)) { (result: Result<Api.Types.Response.CoverLookup, Api.Types.Error>) in
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let success):
+                        var fileName: String = ""
+                        fileName = success.data.first(where: { cover in cover.attributes.locale == "en" })?.attributes.fileName ?? ""
+                        if fileName == "" {
+                            fileName = success.data.first?.attributes.fileName ?? ""
+                        }
+                        manga.coverUrl += fileName
+                        self.manga.append(manga)
+                    case .failure(let failure):
+                        print(failure.localizedDescription)
+                    }
                 }
             }
-        
-        return url
     }
-    
+        
     func getManga(query: String?) {
+        manga.removeAll()
         Api.Sdk.shared
             .get(.manga(query: query)) { (result: Result<Api.Types.Response.MangaSearch, Api.Types.Error>) in
                 DispatchQueue.main.async {
                     switch result {
                     case .success(let success):
-                        self.convertToManga(success)
+                        for result in success.data {
+                            self.appendWithCoverUrl(manga: Manga(id: result.id,
+                                                                 title: result.attributes.title.en,
+                                                                 description: result.attributes.description.en,
+                                                                 coverUrl: "https://uploads.mangadex.org/covers/\(result.id)/"))
+                    }
                     case .failure(let failure):
                         print(failure.localizedDescription)
                     }
