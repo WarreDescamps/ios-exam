bb198bbd5df106b3c2c8e8989df54b8909113ed8
WarreDescamps <warre.descamps@student.vives.be>
2022-12-10 01:02:03 +0100

    Add & implement MangaDetailView navigation

--- IOS-Project/Views/MangaDetailView.swift
+++ IOS-Project/Views/MangaDetailView.swift
@@ -9,14 +9,32 @@ import SwiftUI
 
 struct MangaDetailView: View {
     @State var manga: Manga
+    var onDismiss: () -> Void
+    
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
     
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: Button(action : {
+                onDismiss()
+                self.mode.wrappedValue.dismiss()
+            }){
+                Label("Back", systemImage: "arrow.left")
+            })
     }
 }
 
 struct MangaDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        MangaDetailView(manga: DebugConstants.worldTrigger)
+        MangaDetailView_PreviewContainer()
+    }
+}
+
+struct MangaDetailView_PreviewContainer: View {
+    @State var isShown: Bool = true
+    
+    var body: some View {
+        MangaDetailView(manga: DebugConstants.worldTrigger, onDismiss: {isShown = false})
     }
 }
--- IOS-Project/Views/MangaGridItem.swift
+++ IOS-Project/Views/MangaGridItem.swift
@@ -9,36 +9,60 @@ import SwiftUI
 
 struct MangaGridItem: View {
     var manga: Manga
+    @Binding var showDetail: Bool
+    let isSelectable: Bool
     
     init(manga: Manga) {
         self.manga = manga
+        self.isSelectable = false
+        var showDetail = false
+        self._showDetail = Binding(get: { showDetail }, set: { showDetail = $0 })
+    }
+    
+    init(manga: Manga, showDetail: Binding<Bool>) {
+        self.manga = manga
+        self.isSelectable = true
+        self._showDetail = showDetail
     }
     
     var body: some View {
-        AsyncImage(url: URL(string: manga.coverUrl),
-                   content: { image in image.resizable() },
-                   placeholder: {
-                        ZStack {
-                            Color.gray
-                            ProgressView()
-                                .progressViewStyle(.circular)
-                        }
-                    })
-        //.aspectRatio(contentMode: .fit)
-        .aspectRatio(0.66, contentMode: .fit)
-        .overlay(alignment: .bottom) {
-            LinearGradient(gradient: Gradient(colors: [.black, .clear]), startPoint: .bottom, endPoint: .top)
-                .frame(height: 90.0)
+        ZStack {
+            AsyncImage(url: URL(string: manga.coverUrl),
+                       content: { image in image.resizable() },
+                       placeholder: {
+                ZStack {
+                    Color.gray
+                    ProgressView()
+                        .progressViewStyle(.circular)
+                }
+            })
+            //.aspectRatio(contentMode: .fit)
+            .aspectRatio(0.66, contentMode: .fit)
+            .overlay(alignment: .bottom) {
+                LinearGradient(gradient: Gradient(colors: [.black, .clear]), startPoint: .bottom, endPoint: .top)
+                    .frame(height: 90.0)
+            }
+            .overlay(alignment: .bottomLeading) {
+                Text(manga.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .padding([.leading, .bottom], 5.0)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .if(!isSelectable) { view in
+                view
+                    .onTapGesture {
+                        showDetail = true
+                    }
+            }
+            
+            NavigationLink("Hidden", isActive: $showDetail) {
+                MangaDetailView(manga: manga, onDismiss: { showDetail = false })
+            }
+            .hidden()
         }
-        .overlay(alignment: .bottomLeading) {
-            Text(manga.title)
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-                .multilineTextAlignment(.leading)
-                .lineLimit(2)
-                .padding([.leading, .bottom], 5.0)
-        }
-        .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
@@ -46,12 +70,13 @@ struct SelectableMangaGridItem: View {
     var manga: Manga
     @Binding var selectionState: ViewState
     @Binding var selectedManga: [Manga]
+    @State var showDetail = false
     
     var body: some View {
-        MangaGridItem(manga: manga)
+        MangaGridItem(manga: manga, showDetail: $showDetail)
             .if(selectionState == .viewing) { view in
                 view
-                    .onTapGesture { }
+                    .onTapGesture { showDetail = true }
                     .onLongPressGesture() {
                         DispatchQueue.main.async {
                             selectedManga.removeAll()
@@ -73,7 +98,7 @@ struct SelectableMangaGridItem: View {
                         }
                     }
             }
-            .if(selectedManga.contains(where: { $0.id == manga.id })) { view in
+            .if(selectionState == .selecting && selectedManga.contains(where: { $0.id == manga.id })) { view in
                 view
                     .overlay(alignment: .center) {
                         RoundedRectangle(cornerRadius: 8)
